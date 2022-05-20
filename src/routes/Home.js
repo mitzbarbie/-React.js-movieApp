7418b40fe1ae611a7ddc6e9643550abf53a28405
SeungHwan Hong <mitzbarbie@naver.com>
2022-05-20 10:42:26 +0900

    feat : routes divides

---
+++ src/routes/Home.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading!!!!!</h1>
+      ) : (
+        <div>
+          {movies.map((m) => (
+            <Movie
+              key={m.id}
+              movieImg={m.medium_cover_image}
+              title={m.title}
+              year={m.year}
+              summary={m.summary}
+              genres={m.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
