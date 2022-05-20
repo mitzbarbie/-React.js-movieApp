c8f94a5e7620285a340313f5d20589f659567f11
SeungHwan Hong <mitzbarbie@naver.com>
2022-05-20 11:01:38 +0900

    feat : router & link

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ movieImg, title, year, summary, genres }) {
   return (
     <div>
       <img src={movieImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <strong>{year}</strong>
       <p>{summary}</p>
       <ul>
